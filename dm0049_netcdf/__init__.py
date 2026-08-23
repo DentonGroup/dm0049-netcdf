f2b021a29a834ecc0700778411298b06a521363e
jakeogh <github.com@v6y.net>
2026-08-23 02:42:59 -0700

    dm0049-netcdf: export the upgrade and the renderer
    The names upgrade.py and describe.py provide were never added to the package,
    so importing any of them failed and the suite could not collect. Group the
    exports by module while they are all being touched.

--- dm0049_netcdf/__init__.py
+++ dm0049_netcdf/__init__.py
@@ -6,16 +6,16 @@ from .access import NetCDFGatewayClosed as NetCDFGatewayClosed
 from .access import NetCDFThreadViolation as NetCDFThreadViolation
 from .access import on_gateway_thread as on_gateway_thread
 from .access import run as run
+
 from .container import ACQUIRED_KEY as ACQUIRED_KEY
 from .container import ARRAY_KEYS as ARRAY_KEYS
-from .describe import render as render
-from .container import _scalar as _scalar
 from .container import CONTAINER_FLATTEN_KEYS as CONTAINER_FLATTEN_KEYS
 from .container import FORMAT_VERSION as FORMAT_VERSION
 from .container import FORMAT_VERSION_KEY as FORMAT_VERSION_KEY
 from .container import LEGACY_FORMAT_VERSION_KEY as LEGACY_FORMAT_VERSION_KEY
 from .container import SEED_KEYS as SEED_KEYS
 from .container import SPECTRUM_KEY as SPECTRUM_KEY
+from .container import _scalar as _scalar
 from .container import build_dataset as build_dataset
 from .container import dataset_to_dict as dataset_to_dict
 from .container import flatten_dict as flatten_dict
@@ -24,3 +24,11 @@ from .container import spectrum_dataset as spectrum_dataset
 from .container import unflatten_dict as unflatten_dict
 from .container import ureg as ureg
 from .container import write_acquisition as write_acquisition
+
+from .describe import render as render
+
+from .upgrade import CannotUpgrade as CannotUpgrade
+from .upgrade import LEGACY_ACQUIRED_KEY as LEGACY_ACQUIRED_KEY
+from .upgrade import backup_path as backup_path
+from .upgrade import needs_upgrade as needs_upgrade
+from .upgrade import upgrade_file as upgrade_file
